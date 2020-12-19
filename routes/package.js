f5e1bd1b648f8e23ef19f73dbcc5e81dfa8fd193
humzajamil <SP17-BCS-075@ISBSTUDENT.COMSATS.EDU.PK>
2020-12-20 02:41:13 +0500

    bug fixes 2

--- routes/package.js
+++ routes/package.js
@@ -91,7 +91,7 @@ router.put("/:id", auth, async (req, res) => {
     const buff = Buffer.from(picture.data, "base64");
     packageFields.picture = { type: picture.type, data: buff };
   }
-  if (status !== undefined) serviceFields.status = status;
+  if (status !== undefined) packageFields.status = status;
   if (description) packageFields.description = description;
 
   try {
